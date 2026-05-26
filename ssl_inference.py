1d9011bc41946b3b2e29c170d4a17b749f74ae2e
MKHan91 <audrbz@naver.com>
2026-05-26 15:42:02 +0000

    v1.3.2_LTU: Add FlowAtt module for Label-to-Unlabel Knowledge transfer
    - Implement StarNet-based feature refinement
    - Use XCA to transfer domain knowledge (Label) to unlabeled data
    - Apply flow attention to label feature
    - Convert ASPP to depthwise based ASPP
    - Loss NaN issue clear
    - Convert Polynomial Decay to CosineDecayRestart
    	- step_size: 400, t_mul: 1., m_mul:0.5
    - Add inference code

--- ssl_inference.py
+++ ssl_inference.py
@@ -17,7 +17,7 @@ from model.semseg.deeplabv3plus import DeepLabV3Plus
 from configuration import TestConfig, DataConfig, ModelConfig
 from util.utils import intersectionAndUnion, AverageMeter
 import pandas as pd
-
+import cityscapesLabels as labels
 
 
 def color_map():
@@ -49,6 +49,11 @@ def color_map():
 def main():
     model_name = os.listdir(tcfg.model_save_dir)[-1]
     model_path = osp.join(tcfg.model_save_dir, model_name)
+    result_dir = osp.join(osp.dirname(__file__), 'results') 
+    os.makedirs(osp.join(result_dir, 'rgb'), exist_ok=True)
+    os.makedirs(osp.join(result_dir, 'gt'), exist_ok=True)
+    os.makedirs(osp.join(result_dir, 'pred'), exist_ok=True)
+    
     
     model = DeepLabV3Plus(tcfg, mcfg, pretrained_path='')
     checkpoint = torch.load(model_path)
@@ -62,17 +67,13 @@ def main():
                          size=tcfg.crop_size)
     valloader = DataLoader(valset, batch_size=1, pin_memory=True, num_workers=4, drop_last=False)
 
-    os.makedirs(osp.join(osp.dirname(__file__), 'visual'), exist_ok=True)
     
-    df = pd.DataFrame()
-    intersection_meter = AverageMeter()
-    union_meter = AverageMeter()
-    
+    results = []
+    class_names = [l.name for l in labels.labels if l.trainId != 255]
     model.eval()
     with torch.no_grad():
         for img, mask, image_path in valloader:
             img = img.cuda(non_blocking=True)
-            h, w = img.shape[2:]
             
             res = model(img, mode='test')
             pred = res['out']
@@ -88,29 +89,41 @@ def main():
                 file_name = osp.split(image_path[i])[-1][:-4]
                 print(f"file_name: {file_name:<10}")
 
+                rgb             = img[i].cpu().numpy().transpose(1, 2, 0)
+                rgb             = (rgb * 255).astype(np.uint8)
                 mask_pred_i     = pred_mask[i]
                 mask_i          = mask[i]
-                # mask_filter_i   = mask_fliter[i]
                 
+                # =================================================
+                # Quantative Evaluation
+                # =================================================
                 intersection, union, target = intersectionAndUnion(mask_pred_i.cpu().numpy(), mask_i.cpu().numpy(), mcfg.num_classes, 255)
-                intersection_meter.update(intersection)
-                union_meter.update(union)
                 
+                iou_class_p_img = intersection / (union + 1e-10)
+                row_data = {'image_name': file_name, 'miou_p_img': np.mean(iou_class_p_img) * 100}
+                for class_name, iou in zip(class_names, iou_class_p_img):
+                    row_data[class_name] = iou
+                results.append(row_data)
+                
+                
+                # =================================================
+                # Qualitative Evaluation
+                # =================================================
+                rgb             = Image.fromarray(rgb)
                 mask_i          = Image.fromarray(mask_i.cpu().numpy().astype(np.uint8), mode='P')
                 mask_pred_i     = Image.fromarray(mask_pred_i.cpu().numpy().astype(np.uint8), mode='P')
-                # mask_filter_i   = Image.fromarray(mask_filter_i.cpu().numpy().astype(np.uint8), mode='P')
                 
                 platte = color_map()
                 mask_i.putpalette(platte)
                 mask_pred_i.putpalette(platte)
-                # mask_filter_i.putpalette(platte)
                 
-                mask_i.save(osp.join(osp.dirname(__file__), 'visual', f'{file_name}_mask_gt.png'))
-                mask_pred_i.save(osp.join(osp.dirname(__file__), 'visual', f'{file_name}_mask_pred.png'))
-                # mask_filter_i.save(osp.join(osp.dirname(__file__), 'visual', f'{file_name}', f'{file_name}_mask_filter.png'))
+                rgb.save(osp.join(result_dir, 'rgb', f'{file_name}_rgb.png'))
+                mask_i.save(osp.join(result_dir, 'gt', f'{file_name}_mask_gt.png'))
+                mask_pred_i.save(osp.join(result_dir, 'pred', f'{file_name}_mask_pred.png'))
+                
+        df = pd.DataFrame(results)
+        df.to_csv(osp.join(result_dir, 'evaluation_results.csv'), index=False)
 
-        
-        iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
 
 if __name__ == "__main__":
     dcfg = DataConfig()
